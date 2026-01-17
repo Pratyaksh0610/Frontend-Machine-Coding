7e7515d9d94df8dc18ce1bde860085c01c74bb72
Pratyaksh0610 <pratyaksh0610@gmail.com>
2026-01-17 23:47:50 +0530

    Grid question

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,10 @@
 import "./App.css";
-import Accordion from "./components/Accordion/Accordion";
-import AutocompleteSearchBar from "./components/AutocompleteSearchBar/AutocompleteSearchBar";
-import EMICalculator from "./components/EMICalculator/EMICalculator";
-import EmployeeDBMS from "./components/EmployeeDBMS/EmployeeDBMS";
-import FileExplorer from "./components/FileExplorer/FileExplorer";
-import FileStructure from "./components/FileExplorer/FileStructure";
-import NestedCheckbox from "./components/NestedCheckbox/NestedCheckbox";
-import OTPInput from "./components/OTPInput/otpInput";
-import PasswordGenerator from "./components/PasswordGenerator/PasswordGenerator";
-import ProgressBar from "./components/ProgressBar/ProgressBar";
-import TodoList from "./components/TodoList/TodoList";
-import { CheckboxData, FileStructureData } from "./constants/componentConstants";
-// import Pagination from './components/Pagination/Pagination'
-// import Tab from './components/TabsForm/Tab'
+import GridLights from "./components/GridQuestions/GridLights/GridLights";
 
 function App() {
   return (
     <>
-      {/* <Tab/> */}
-      {/* <Pagination/> */}
-      {/* <AutocompleteSearchBar /> */}
-      {/* <FileStructure /> */}
-      {/* <ProgressBar progress={80}/> */}
-      {/* <OTPInput inputSize={5} /> */}
-      {/* <NestedCheckbox data={CheckboxData} /> */}
-      {/* <TodoList/> */}
-      {/* <Accordion/> */}
-      {/* <EmployeeDBMS/> */}
-      {/* <EMICalculator/> */}
-      <PasswordGenerator/>
+      <GridLights />
     </>
   );
 }
